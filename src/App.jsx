3f82680fc7f2082c8efe3928daffdd2d2b307f16
kebal khadka <kebalkhadka7@gmail.com>
2024-11-27 22:55:06 +0545

    deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
 import React from 'react'
 
 
@@ -11,12 +9,7 @@ function App() {
 
   return (
     <>
-    <div className='h-screen bg-black'>
-      <div className='h-[90%] flex '>
-        <Sidebar/>  
-      </div>
-     <Player/>
-    </div>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import MusicProvider from './context/MusicContext.jsx'
 import React from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MusicProvider>
+
     <App />
-    </MusicProvider>
+  
    
   </StrictMode>,
 )
